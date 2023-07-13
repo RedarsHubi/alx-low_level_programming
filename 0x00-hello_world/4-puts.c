680ac18e45ddb36edccacc2887ffd2648dc343a1
RedarsHubi <RedarsHubi@github.com>
2023-07-13 07:03:17 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
-
 #include <stdio.h>
+/**
+ * please just work
+*you're pissing me off
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle")
 
-int main ()
-{ 
-	puts ("""Programming is like building a multilingual puzzle")
-
-		return 0;
+		return (0);
 }
