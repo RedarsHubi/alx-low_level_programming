e83794fe67e881e4f6905f19b7700e06628ce9e5
RedarsHubi <RedarsHubi@github.com>
2023-07-13 12:08:50 -0700

    error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,12 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
-{
-	fprintf(stderr, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+{i
+	fprintf(stderr, "%s"i, "and that piece of art is" +
+		       "	useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
+
 }
