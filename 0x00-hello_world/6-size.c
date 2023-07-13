6f44e2616d58e8c2e5b6f4ee1167ae610d923ac3
RedarsHubi <RedarsHubi@github.com>
2023-07-13 08:45:09 -0700

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,5 +11,5 @@ int main(void)
 	printf("Size of a long int: %ld bytes(s)\n", sizeof(long int));
 	printf("Size of a long long int: %ld bytes(s)\n", sizeof(long long int));
 	printf("Size of a float: %ld byte(s)", sizeof(float));
-			return (0);
+	return (0);
 }
