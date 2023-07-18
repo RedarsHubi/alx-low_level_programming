2fa78847b3eaf08be375444ebb4d976102ba3d92
RedarsHubi <RedarsHubi@github.com>
2023-07-18 05:39:26 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	char word[] = "_putchar";
+
+	for (i = 0; word[i] != '\0'; i++)
+	{
+		putchar(word[i]);
+	}
+	putchar('\n');
+	return (0);
+}
