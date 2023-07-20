58468590fdceb92a96c5c3a3a23572a04df8a833
RedarsHubi <RedarsHubi@github.com>
2023-07-20 08:04:31 -0700

    digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * _isupper - Checks for uppercase character
+  * @c: variable for character checked
+  * Return: 1 if uppercase, 0 otherwise
+  **/
+
+int _isupper(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
