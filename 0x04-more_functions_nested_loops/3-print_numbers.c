2aa143d793beba7df428f8a5b2ffd66143558b74
RedarsHubi <RedarsHubi@github.com>
2023-07-20 08:42:30 -0700

    3numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * print_numbers - prints digits from 0 to 9
+  *
+  * Return: digits
+  **/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 58; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
