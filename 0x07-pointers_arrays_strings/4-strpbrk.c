2ae1db9517d23c4e4df5dbc9cfaf8c2b49ef7c70
RedarsHubi <RedarsHubi@github.com>
2023-07-31 12:04:29 -0700

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+  * _strpbrk - function that loactes character
+  * in a string.
+  * @s: string to check
+  * @accept: character to be encountered.
+  * Return: location of first encounter, otherwise NULL.
+  **/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0;
+	int j = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (s);
+			}
+		}
+	}
+	return (NULL);
+}
