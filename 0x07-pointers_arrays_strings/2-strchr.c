a7d26a69324bbe3514c3e797aff6665b45663dc4
RedarsHubi <RedarsHubi@github.com>
2023-07-31 06:32:40 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * _strchr - function that locates first occurence of a character
+  * in a string.
+  * @s: pointer
+  * @c: character to be found
+  * Return: Pointer to modified area
+  **/
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			break;
+		}
+		if (s[i] == '\0')
+		{
+			return ('\0');
+		}
+	}
+	return (s + 1);
+}
