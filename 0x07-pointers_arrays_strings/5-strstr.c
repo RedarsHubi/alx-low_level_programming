17a4b87edba7482b6895f1723949501b7c906ef8
RedarsHubi <RedarsHubi@github.com>
2023-07-31 12:47:41 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+  * _strstr - function that loactes character
+  * in a string.
+  * @haystack: string to checked
+  * @needle: substring to be found
+  * Return: location of first encounter, otherwise NULL.
+  **/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+	int j = 0;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i] == needle[j])
+			{
+				return (haystack + i);
+			}
+		}
+	}
+	return (NULL);
+}
