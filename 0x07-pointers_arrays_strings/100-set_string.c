d9bc19fcad388a7c6a470988e4d5e0a78ddca89d
RedarsHubi <RedarsHubi@github.com>
2023-07-31 14:02:50 -0700

    setchar

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - changes value of char to str
+ * @s: source.
+ * @to: target.
+ * Return: no return.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
