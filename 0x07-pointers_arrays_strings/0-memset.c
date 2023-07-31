6c39249a9e4cf9c44e6e5d115ca4a99fd3c0307c
RedarsHubi <RedarsHubi@github.com>
2023-07-31 05:37:58 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * _memset - function that fills memory area with
+  * constant bytes.
+  * @*s: pointer
+  * @b: bytes
+  * @n: number of filled locations
+  * Return: Pointer to modified area
+  **/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
