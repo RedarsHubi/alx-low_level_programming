65f2beafa08c5f07f55d80279b8675ef8fd14da9
RedarsHubi <RedarsHubi@github.com>
2023-08-02 08:52:37 -0700

    pow

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * _sqrt_recursion - returns sqrt of natural numb.
+  * @n: number.
+  * Return: sqrt of n.
+  **/
+
+int _sqrt_recursion(int n)
+{
+	int i;
+
+	if (i * i == n && i > 0)
+	{
+		return (i);
+	}
+	else if
+	{
+		return _sqrt_recursion(n / (i +1);
+	}
+	else
+	{
+		return (-1);
+	}
+}
