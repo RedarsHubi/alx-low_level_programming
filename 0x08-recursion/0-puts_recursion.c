b8d891807f4663b3dad9795a48b012684ce7b452
RedarsHubi <RedarsHubi@github.com>
2023-08-02 05:46:46 -0700

    string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * _puts_recursion - prints a string.
+  * @s: pointer to string to be printed.
+  * Return: no return.
+  **/
+
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	if (*(s + 1) != '\0')
+	{
+		_puts_recursion(s + 1);
+	}
+}
