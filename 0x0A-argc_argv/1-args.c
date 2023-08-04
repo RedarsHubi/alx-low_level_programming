2fb0c96d1f33dce5163c313f6b380c6f64e0e877
RedarsHubi <RedarsHubi@github.com>
2023-08-04 05:45:35 -0700

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * main - main function
+  * @argc: integer that that counts args.
+  * @argv: pointer to argument strings
+  * Return: no return
+  **/
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	(void)argv;
+
+	return (0);
+}
