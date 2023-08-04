0b3734f1234ae2bff08129926656ad2e28f4e27a
RedarsHubi <RedarsHubi@github.com>
2023-08-04 09:56:39 -0700

    library

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,1 +1,32 @@
-char *_strstr(char *haystack, char *needle);
+#include "main.h"
+#include <stddef.h>
+
+/**
+  * _strstr - function that loactes character
+  * in a string.
+  * @haystack: string to checked
+  * @needle: substring to be found
+  * Return: location of first encounter, otherwise NULL.
+  **/
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *a;
+	char *b;
+
+	while (*haystack != '\0')
+	{
+		a = haystack;
+		b = needle;
+
+		while (*haystack != '\0' && *b != '\0' && *haystack == *b)
+		{
+			haystack++;
+			b++;
+		}
+		if (!*b)
+			return (a);
+		haystack = a + 1;
+	}
+	return (0);
+}
--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,1 +1,17 @@
-int _abs(int n);
+#include "main.h"
+/**
+*  _abs - computes an absolute number
+* @i: number as an integer
+* Return: absolute value
+**/
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
