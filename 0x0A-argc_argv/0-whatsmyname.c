dbc9cda58e6dcea22880fab49d5e631b73d5e134
RedarsHubi <RedarsHubi@github.com>
2023-08-04 05:41:37 -0700

    whatmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * main - main function
+  * @argc: integer that that counts args.
+  * @argv: pointer to argument strings
+  * Return: no return
+  **/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+
+	return (0);
+}
