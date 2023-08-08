a0a63be05bbb76cfcd49e89838f63755c93fcdce
RedarsHubi <RedarsHubi@github.com>
2023-08-08 03:46:55 -0700

    cat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+  * str_concat - appends a string to another
+  * @s1: string
+  * @s2: string
+  * Return: location of appended strings, otherwise NULL
+  **/
+
+char *str_concat(char *s1, char *s2)
+{
+	int i, j, k;
+	char *con;
+
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[i] != '\0'; i++)
+		;
+	con = malloc(sizeof(char) * (i + j + 1);
+	if (con == NULL)
+	{
+		return (NULL);
+	}
+	for (k = 0; k < i; k++)
+	{
+		con[k] = s1[k];
+	}
+	for (k = 0; k <= j; k++)
+	{
+		con[i + k] = s2[k];
+	}
+	return (con);
+}
