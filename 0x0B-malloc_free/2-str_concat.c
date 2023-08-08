2a857e650c2e459ebf5209252290eb2c0e71822d
RedarsHubi <RedarsHubi@github.com>
2023-08-08 04:05:12 -0700

    cat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -25,11 +25,11 @@ char *str_concat(char *s1, char *s2)
 	}
 	if (s1 == NULL)
 	{
-		return ("");
+		s1 = "";
 	}
 	if (s2 == NULL)
 	{
-		return ("");
+		s2 = ("");
 	}
 	for (k = 0; k < i; k++)
 	{
