24e19d80f7ce43d20df30b5cee8d9764959d713e
RedarsHubi <RedarsHubi@github.com>
2023-08-08 07:18:16 -0700

    argstostr

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,12 +13,6 @@ char *str_concat(char *s1, char *s2)
 	int i, j, k;
 	char *con;
 
-	for (i = 0; s1[i] != '\0'; i++)
-		;
-	for (j = 0; s2[j] != '\0'; j++)
-		;
-	con = malloc(sizeof(char) * (i + j + 1));
-
 	if (con == NULL)
 	{
 		return (NULL);
@@ -31,6 +25,12 @@ char *str_concat(char *s1, char *s2)
 	{
 		s2 = "";
 	}
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+	con = malloc(sizeof(char) * (i + j + 1));
+
 	for (k = 0; k < i; k++)
 	{
 		con[k] = s1[k];
