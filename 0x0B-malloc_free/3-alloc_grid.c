7ef87c9ff24051ee93cd5e2017e2fcf44143b1ce
RedarsHubi <RedarsHubi@github.com>
2023-08-08 06:19:31 -0700

    grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+  * alloc_grid - function that returns ptr to 2D array
+  * @width: columnns
+  * @height: rows
+  * Return: pointer to a 2D array, otherwsie NULL
+  **/
+
+int **alloc_grid(int width, int height)
+{
+	int **arr;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	arr = malloc(sizeof)(int *) * height;
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = malloc(sizeof)(int) * width;
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			arr[i][j] = 0;
+		}
+	}
+	return (arr);
+}
