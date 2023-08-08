7d142da0266643288d424a13c7644137bb1b1942
RedarsHubi <RedarsHubi@github.com>
2023-08-08 09:03:26 -0700

    catall

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -12,41 +12,4 @@ char *argstostr(int ac, char **av)
 	char *arr;
 	int i, j, in, z;
 
-	if (ac == 0)
-	{
-		return (NULL);
-	}
-	for (i = 0; i < ac; i++)
-	{
-		if (av[i] == NULL)
-		{
-			return (NULL);
-		}
-		for (j = 0; av[i][j] != '\0'; j++)
-		{
-			in++;
-		}
-		in++;
-	}
-	arr = malloc((in + 1) * sizeof(char));
-	if (arr == NULL)
-	{
-		return (NULL);
-	}
-	for (z = 0; z <= in; j++, z++)
-	{
-		if (av[i][j] == '\0')
-		{
-			arr[z] = '\n';
-			i++;
-			z++;
-			j = 0;
-		}
-		if (z < in - 1)
-		{
-			arr[z] = av[i][j];
-		}
-		arr[z] = '\0';
-	}
-	return (arr);
-}
+
