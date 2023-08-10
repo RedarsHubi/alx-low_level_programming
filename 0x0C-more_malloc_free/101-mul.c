0cc486bacfebfd859b41f92ef43cc313bcae36d8
RedarsHubi <RedarsHubi@github.com>
2023-08-10 07:52:16 -0700

    mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -11,20 +11,31 @@
 
 int main(int argc, char *argv[])
 {
+	int i, j;
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+	int result = num1 * num2;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (98);
 	}
-	else
+
+	for (i = 1; i < argc; i++)
 	{
-		int num1 = atoi(argv[1]);
-		int num2 = atoi(argv[2]);
+		char *str = argv[i];
 
-		int result = num1 * num2;
+		for (j = 0; str[j] != '\0'; j++)
+		{
+			if (str[j] < 0 || str[j] > 9)
+			{
+				printf("Error\n");
+				return (98);
+			}
+		}}
+	printf("%d\n", result);
 
-		printf("%d\n", result);
 
-	}
 	return (0);
 }
