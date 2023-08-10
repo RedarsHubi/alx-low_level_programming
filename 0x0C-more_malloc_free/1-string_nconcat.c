29e71ffca545f371aac2885e5397d2829632588b
RedarsHubi <RedarsHubi@github.com>
2023-08-10 05:44:55 -0700

    concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -44,11 +44,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			arr[k] = s2[k - i];
 		}
 	}
-
+	arr[k] = '\0';
 	if (arr == NULL)
 	{
 		return (NULL);
 	}
-	arr[k] = '\0';
 	return (arr);
 }
