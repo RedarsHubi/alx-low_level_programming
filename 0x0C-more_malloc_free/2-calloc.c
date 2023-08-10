3bb97cb78004f711f3ec42bec4bd85233710650f
RedarsHubi <RedarsHubi@github.com>
2023-08-10 06:03:37 -0700

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - returns pointer to arrray
+ * @nmemb: int
+ * @size: int
+ * Return: pointer to array, NULL if failure
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *arr;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	arr = malloc(nmemb * size * sizeof(char));
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = 0;
+	}
+	return (arr);
+}
