87b7191c8445dc8c74b567429921d313b6260617
RedarsHubi <RedarsHubi@github.com>
2023-08-10 07:18:27 -0700

    calloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _realloc - reallocates memory size
+ * @old_size: integer
+ * @new_size: integer
+ * @ptr: pointer
+ * Return: pointer to new array with new  memory size
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+
+	int *arr;
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	if (ptr == NULL)
+	{
+		return (malloc(new_size));
+	}
+	arr = malloc(new_size);
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	free(ptr);
+	return (arr);
+}
