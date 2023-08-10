3fed2f2ba47f50bbaa259aa5dbadbb551c9af81e
RedarsHubi <RedarsHubi@github.com>
2023-08-10 08:52:15 -0700

    mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,111 @@
 #include <stdio.h>
 #include "main.h"
 #include <stdlib.h>
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _puts - prints a string, followed by a new line.
+ * @str: input string.
+ * Return: no return.
+ */
+
+void _puts(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[count]);
+		count++;
+	}
+}
+
+/**
+ * _atoi - converts a string to an integer.
+ * @s: input string.
+ * Return: integer.
+ */
+
+int _atoi(char *s)
+{
+	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
+
+	while (*(s + count) != '\0')
+	{
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+			break;
+
+		if (*(s + count) == '-')
+			pn *= -1;
+
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		{
+			if (size > 0)
+				m *= 10;
+			size++;
+		}
+		count++;
+	}
+
+	for (i = count - size; i < count; i++)
+	{
+		oi = oi + ((*(s + i) - 48) * m);
+		m /= 10;
+	}
+	return (oi * pn);
+}
+
+/**
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
+ */
+
+void print_number(int n)
+{
+	unsigned int m, d, count;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		m = n * -1;
+	}
+	else
+	{
+		m = n;
+	}
+
+	d = m;
+	count = 1;
+
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
+}
 
 /**
   * main - main function
@@ -11,10 +116,10 @@
 
 int main(int argc, char *argv[])
 {
-	int i, j;
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-	int result = num1 * num2;
+	long int i, j;
+	long int num1 = _atoi(argv[1]);
+	long int num2 = _atoi(argv[2]);
+	long int result;
 
 	if (argc != 3)
 	{
@@ -28,13 +133,15 @@ int main(int argc, char *argv[])
 
 		for (j = 0; str[j] != '\0'; j++)
 		{
-			if (str[j] < 0 || str[j] > 9)
+			if (str[j] < '0' || str[j] > '9')
 			{
-				printf("Error\n");
+				_puts("Error\n");
 				return (98);
 			}
 		}}
-	printf("%d\n", result);
+	result = num1 * num2;
+	print_number(result);
+	_putchar('\n');
 
 	return (0);
 }
