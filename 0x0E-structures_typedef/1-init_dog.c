c37ec67d0960a996840dd5a1fb8e43d08749d0fd
RedarsHubi <RedarsHubi@github.com>
2023-08-14 06:46:08 -0700

    init_dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,7 +1,5 @@
 #include "dog.h"
 
-#include "dog.h"
-
 /**
  * init_dog - initializes funtcion
  * @d: structure
@@ -15,6 +13,6 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d->name = name;
-	d.age = age;
+	d->age = age;
 	(*d).owner = owner;
 }
