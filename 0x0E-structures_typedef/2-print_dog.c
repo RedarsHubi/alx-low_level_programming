2a05d875066b6d7f6bd7a5e2bfa5c20abc257ac7
RedarsHubi <RedarsHubi@github.com>
2023-08-14 07:46:36 -0700

    print_dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,35 @@
+#include "dog.h"
+
+/**
+ * init_dog - prints func
+ * @d: pointer
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name = NULL)
+		{
+			printf("Name: (nil)");
+		}
+		else
+		{
+			printf("Name: %s\n", d->name);
+		}
+		if (d->age = NULL)
+		{
+			printf("Age: (nil)");
+		}
+		else
+		{
+			printf("Age : %.6f\n", d->age);
+		}
+		if (d->owner = NULL)
+		{
+			printf("Owner: (nil)");
+		}
+		else
+		{
+			printf("Owner: %s\n", d->owner);
+		}
+	}
