7d4f62af1b7a1e7de0f7cb108c2e1af39d8e2457
RedarsHubi <RedarsHubi@github.com>
2023-08-16 04:42:32 -0700

    printname

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,27 +1,15 @@
 #include "function_pointers.h"
 
 /**
-  * f - function that prints name
-  * @str: string
-  */
-
-void f(char *str)
-{
-	int i = 0;
-
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i++;
-	}
-
-}
-/**
   * print_name - function that prints name
   * @name: string
   * @f: function that takes string
   */
+
 void print_name(char *name, void (*f)(char *))
 {
-	(*f) (name);
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
 }
