a9d4803aac1f3a56a56cafabddef98f605478ce5
RedarsHubi <RedarsHubi@github.com>
2023-08-17 09:07:22 -0700

    string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -27,7 +27,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		printf("%s", store);
 		}
 		else
-		printf("(nil");
+		printf("(nil)");
 		if (i != (n - 1) && separator != NULL)
 		{
 		printf("%s", separator);
