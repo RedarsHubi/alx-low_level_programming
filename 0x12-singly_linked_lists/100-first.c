3e6325c329213a9a844c86e95424bf759a67c5b3
RedarsHubi <RedarsHubi@github.com>
2023-08-24 08:26:34 -0700

    print before mainx

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * beatfunc - prints a string
+ * Return: void
+ */
+
+void beatfunc(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n);
+}
