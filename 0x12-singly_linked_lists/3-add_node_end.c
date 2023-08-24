384d61dd1e63297e4f6e9b525b7a291a3965b547
RedarsHubi <RedarsHubi@github.com>
2023-08-24 06:57:52 -0700

    node at the end

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+  * add_node_end - adds node at the end
+  * @head: pointer to node
+  * @str: string
+  * Return: address of new node, otherwise NULL
+  */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *newnode = malloc(sizeof(list_t));
+
+	while (newnode != NULL)
+	{
+		newnode->str = strdup(str);
+		newnode->len = strlen(str);
+		list_t *new = *head;
+
+		while (new->next != NULL)
+		{
+			new = new->next;
+		}
+		newnode->next = new;
+
+		return (newnode);
+	}
+	return (NULL);
+}
