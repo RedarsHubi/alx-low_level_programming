51b2b0c2f775152f147c531b25e8590e9003326e
RedarsHubi <RedarsHubi@github.com>
2023-08-24 08:19:19 -0700

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+  * free_list - free list_t
+  * @head: pointer to first node
+  * Return: void
+  */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+		temp = NULL;
+	}
+}
