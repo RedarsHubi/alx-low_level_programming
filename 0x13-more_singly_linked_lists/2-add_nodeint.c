ebdb713e2185215ed16a5632069463eceafa2b7c
RedarsHubi <RedarsHubi@github.com>
2023-08-28 04:26:41 -0700

    node beginning

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -13,3 +13,15 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *arr;
 
+	arr = head;
+	arr = malloc(sizeof(listint_t));
+
+	if (arr == NULL)
+		return (NULL);
+
+	arr->n = n;
+	arr->next = *head;
+
+	*head = arr;
+	return (arr);
+}
