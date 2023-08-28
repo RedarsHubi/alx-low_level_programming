096ff1578d2b05e0e895c212419cbd162a602261
RedarsHubi <RedarsHubi@github.com>
2023-08-28 03:53:35 -0700

    print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+  * print_listint - prints all elements of a list
+  * @h: header
+  * Return: numbers of nodes
+  */
+
+
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *arr;
+	int count;
+
+	arr = h;
+
+	while (arr != NULL)
+	{
+		printf("%d\n", arr->n);
+		arr = arr->next;
+		count++;
+	}
+	return (count);
+}
