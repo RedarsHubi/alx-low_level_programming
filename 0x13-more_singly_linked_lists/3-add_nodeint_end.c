992db9ee37672e66666e46115717be22018e412e
RedarsHubi <RedarsHubi@github.com>
2023-08-28 08:30:56 -0700

    add node end

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,7 +13,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *arr;
 
-	listint_t *current = *head;
+	listint_t *current;
 
 	arr = malloc(sizeof(listint_t));
 
@@ -27,6 +27,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		arr = *head;
 	else
 	{
+		current = *head;
 		while (current->next != NULL)
 			current = current->next;
 		current->next = arr;
