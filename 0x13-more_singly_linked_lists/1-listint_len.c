a9bb16b100fa8953274ded3f63d1a91963c0f783
RedarsHubi <RedarsHubi@github.com>
2023-08-28 08:12:01 -0700

    list len

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -7,7 +7,7 @@
   * Return: numbers of nodes
   */
 
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	const listint_t *arr;
 	int count;
---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+  * pop_listint - deletes the head node of a list
+  * @head: head of node
+  * Return: head node's data, otherwise 0.
+  */
+
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int store = temp->n;
+
+	temp = *head;
+	head = head->next;
+	free(temp);
+	temp = NULL;
+	if (*head = NULL)
+		return (0);
+	return (store);
+}
