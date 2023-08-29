31504688d11bd15ca4b2e4ee2ea0f73dbe05c9d4
RedarsHubi <RedarsHubi@github.com>
2023-08-29 06:35:12 -0700

    delete node at nth

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,7 +11,7 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int n = 0;
-	unsigned int j = 0;
+
 	listint_t *ptr;
 	listint_t *ptr2 = NULL;
 
@@ -25,15 +25,11 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(ptr);
 		return (1);
 	}
-	while (n < index  && ptr != NULL)
+	while (n < index - 1 && ptr != NULL)
 	{
+		ptr2 = ptr;
 		ptr = ptr->next;
 		n++;
-	}
-	while (j < index - 1 && ptr2 != NULL)
-	{
-		ptr2 = ptr2->next;
-		j++;
 	}
 	ptr2->next = ptr->next;
 	free(ptr);
