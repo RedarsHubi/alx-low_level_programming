e35788ffca8275bb3518316614684d8536158353
RedarsHubi <RedarsHubi@github.com>
2023-08-31 03:32:25 -0700

    convert to binary

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+/**
+  * binary_to_uint - converts a binary number to an unsigned int
+  * @b: a string of 0 and 1
+  * Return: convert num or 0
+  */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i = 0;
+	int length;
+	unsigned int num;
+
+	length = strlen(b);
+
+	if (b == NULL)
+		return (0);
+	for (i = 0; i < length; i++)
+	{
+		if (b[i] != 0 || b[i] != 1)
+			return (0);
+		num = num * 2 + (b[i] - '0');
+	}
+	return (num);
+}
