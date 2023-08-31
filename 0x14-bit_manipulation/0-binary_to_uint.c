b65fd4cc2100218f65df48338fe6e96770994424
RedarsHubi <RedarsHubi@github.com>
2023-08-31 03:39:28 -0700

    changed condition operator

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -18,7 +18,7 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	for (i = 0; i < length; i++)
 	{
-		if (b[i] != '0' || b[i] != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 		num = num * 2 + (b[i] - '0');
 	}
