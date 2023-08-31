7a488df259c266923e17712a16666c54dba50c7d
RedarsHubi <RedarsHubi@github.com>
2023-08-31 07:35:49 -0700

    sex index bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,14 +7,14 @@
   * Return: set value of bit at index, otherwise -1.
   */
 
-int set_bit(unsigned long int n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int i;
 
 	if (index >= (sizeof(n) * 8))
 		return (-1);
 	i = 1 << index;
-	if (n & i)
+	if (*n & i)
 		return (1);
 	else
 		return (1);
