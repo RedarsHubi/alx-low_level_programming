60005d9bb0c5d482167fb39b94956204f19b615d
RedarsHubi <RedarsHubi@github.com>
2023-08-31 06:42:31 -0700

    get index bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * get_bit - returns value of index bit
+  * @index: index
+  * @n: number
+  * Return: value of bit at index, otherwise -1.
+  */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int i;
+
+	if (index >= (sizeof(n)))
+		return (-1);
+	i = 1 << index;
+	if (n & i)
+		return (1);
+	else
+		return (0);
+}
