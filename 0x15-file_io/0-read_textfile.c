7f005fae415984a069fd6d7c78211c01647849c5
RedarsHubi <RedarsHubi@github.com>
2023-09-04 06:19:46 -0700

    read & write

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+  * read_textfile - reads text file
+  * @filename: file
+  * @letters: content
+  * Return: returns number of letters otherwise 0
+  */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *poem;
+	int fd = 0;
+	ssize_t store;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDWR);
+	if (fd == -1)
+		return (0);
+
+	poem = malloc(letters);
+
+	if (poem == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	store = read(fd, poem, letters);
+	if (store == -1)
+	{
+		close(fd);
+		return (0);
+	}
+
+	write(1, poem, store);
+	close(fd);
+	return (store);
+}
