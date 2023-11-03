cef9de9d5d606887b0d2278cac14c7ace0145afd
RedarsHubi <RedarsHubi@github.com>
2023-11-03 03:18:39 -0700

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,14 +14,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (table == NULL)
 		return (NULL);
 
-	table->array = malloc(sizeof(hash_table_t) * size);
-	if (table->array == NULL)
+	array = malloc(sizeof(hash_table_t) * size);
+	if (array == NULL)
 	{
-		free(table);
 		return (NULL);
 	}
 	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
+
 	table->size = size;
 	table->array = array;
 
