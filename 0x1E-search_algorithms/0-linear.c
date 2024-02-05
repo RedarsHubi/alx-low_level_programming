35acbd09d09510bab56d83e0af3b9936c8cd34c7
root <redarshubi@github.com>
2024-02-05 02:21:17 -0800

    1

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,6 +11,9 @@ int linear_search(int *array, size_t size, int value)
 {
 	int i = 0;
 
+	if (array == NULL)
+		return (-1);
+
 	for (; i < (int)size; i++)
 	{
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
