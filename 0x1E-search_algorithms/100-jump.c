406dd6439bcc3ead2c552b5d0be51e5826582d6c
root <redarshubi@github.com>
2024-02-07 01:21:48 -0800

    jump

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -12,10 +12,9 @@ int jump_search(int *array, size_t size, int value)
 {
 	int step = sqrt(size);
 	int i = 0;
-	
 
 	if (array == NULL)
-		return(-1);
+		return (-1);
 
 	for (;  i < (int)size && i < value; i += step)
 	{
@@ -26,8 +25,8 @@ int jump_search(int *array, size_t size, int value)
 	for (i = i - step; i < (int)size; i++)
 	{
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
-                if (array[i] == value)
-                        return (i);
+		if (array[i] == value)
+			return (i);
 	}
 	return (-1);
 }
